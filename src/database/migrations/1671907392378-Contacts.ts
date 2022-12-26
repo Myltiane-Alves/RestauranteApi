ab35d059006879397439006e5ab337bd7d4a63a1
Myltiane <myltiane.aux@gmail.com>
2022-12-26 12:41:09 -0300

    feat: criando tabela de contact

--- src/database/migrations/1671907392378-Contacts.ts
+++ src/database/migrations/1671907392378-Contacts.ts
@@ -1,11 +1,60 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class Contacts1671907392378 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'contacts',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'phone',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'message',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'datetime',
+                        default: 'CURRENT_TIMESTAMP',
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: 'datetime',
+                        default: 'CURRENT_TIMESTAMP',
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('contacts');
     }
 
 }
